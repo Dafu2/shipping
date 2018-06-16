e03465607580f815cff435aabab45e2bb3de50ee
gabe-h <gabe.hernandez@slingshotaerospace.com>
2018-06-16 13:11:42 -0700

    boop

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import os
 import sys
+import pandas
+import numpy
+import matplotlib as mpl
+from xml.dom import minidom
 
-print 'test'
-print 'Starting Shipping.io'
 
+
+
+print ('test')
+print ('Starting Shipping.io')
+
+print ('loading weather data')
